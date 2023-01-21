00fca4e2fac5577c56e3a8222f4790c3fc9e0217
Lucca Viccini <lucca.viccini@engenharia.ufjf.br>
2023-01-21 00:53:47 -0300

    feat: creating sessions collection

--- src/app.js
+++ src/app.js
@@ -22,6 +22,55 @@ try {
 
 db = mongoClient.db();
 const USERS_COLLECTION = db.collection("users");
+const SESSIONS_COLLECTION = db.collection("sessions");
+const ENTRIES_COLLECTION = db.collection("entries");
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // check if email and password are valid
+  const schema = joi.object({
+    name: joi.string().min(1).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(1).required(),
+  });
+
+  const { error } = schema.validate({ name, email, password });
+  if (error) {
+    return res.status(422).send("Erro de validação de dados");
+  }
+
+  // check if email or name already exists
+  const user = await USERS_COLLECTION.findOne({
+    $or: [{ email }, { name }],
+  }).catch((err) => {
+    console.log("Erro no findOne", err.message);
+    return res.status(500).send("Internal server error");
+  });
+  if (user) {
+    return res.status(400).send("Email or name already in use");
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  await USERS_COLLECTION.insertOne({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  await ENTRIES_COLLECTION.insertOne({
+    name,
+    balance: 0,
+    entries: [],
+  });
+
+  // send status and token
+  res.status(200).send("User created");
+});
 
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -58,70 +107,116 @@ app.post("/login", async (req, res) => {
   // create token
   const token = uuidV4();
 
-  // update user with token
-  await USERS_COLLECTION.updateOne(
-    {
-      _id: ObjectId(user._id),
-    },
-    {
-      $set: {
-        token,
-      },
-    }
-  );
+  const session ={
+    name: user.name,
+    email: user.email,
+    token,
+  }
+
+  // create session
+  await SESSIONS_COLLECTION.insertOne(session);
 
   // send status and token
-  res.status(200).send(token);
+  res.status(200).send(session);
 
 });
 
-app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
 
-  // check if email and password are valid
+app.post("/entries" , async (req, res) => {
+  //TEM QUE FAZER O TOKEN
+  const {value, description, type, token} = req.body;
+
+  // check if description, value, type and token are valid
   const schema = joi.object({
-    name: joi.string().min(1).required(),
-    email: joi.string().email().required(),
-    password: joi.string().min(1).required(),
+    value: joi.number().min(1).required(),
+    description: joi.string().min(1).required(),
+    type: joi.string().valid("income", "expense").required(),
+    token: joi.string().min(1).required(),
   });
 
-  const { error } = schema.validate({ name, email, password });
+  const { error } = schema.validate({value, description, type, token });
   if (error) {
     return res.status(422).send("Erro de validação de dados");
   }
 
-  // check if email or name already exists
-  const user = await USERS_COLLECTION.findOne({
-    $or: [{ email }, { name }],
+  // check if token is valid
+  const user = await SESSIONS_COLLECTION.findOne({
+    token,  
   })
     .catch((err) => {
       console.log("Erro no findOne", err.message);
       return res.status(500).send("Internal server error");
     })
     ;
-  
-  if (user) {
-    return res.status(400).send("Email or name already in use");
+
+  if (!user) {
+    return res.status(401).send("Token inválido");
   }
 
-  // hash password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
+  // create entry
+  const userEntry = await ENTRIES_COLLECTION.findOne(
+    { name: user.name },
+  );
 
-  // create token
-  const token = uuidV4();
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const dayapi = `${day}/${month}`;
 
-  // create user
-  await USERS_COLLECTION.insertOne({
-    name,
-    email,
-    password: hashedPassword,
-    token,
-  });
+  const newEntry = {
+    date: dayapi,
+    value,
+    description,
+    type,
+  };
 
-  // send status and token
-  res.status(200).send(token);
+  if (type === "income") {
+    userEntry.balance += value;
+  } else {
+    userEntry.balance -= value;
+  }
+
+  userEntry.entries.push(newEntry);
+
+  await ENTRIES_COLLECTION.updateOne(
+    { name: user.name },
+    { $set
+      : { balance: userEntry.balance, entries: userEntry.entries },
+    },
+  );
+
+
+  
+  // send status
+  res.status(200).send("Entry created");
 });
+
+app.get("/entries", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+  if (!token) return res.status(422).send("Informe o token!");
+  console.log(token);
+  // check if token is valid
+  const user = await SESSIONS_COLLECTION.findOne({
+    token,
+  }).catch((err) => {
+    console.log("Erro no findOne", err.message);
+    return res.status(500).send("Internal server error");
+  });
+  if (!user) {
+    return res.status(401).send("Token inválido");
+  }
+
+  // get entries
+  const userEntry = await ENTRIES_COLLECTION.findOne(
+    { name: user.name },
+  );
+
+  // send status and entries
+  res.status(200).send(userEntry);
+});
+
+
 
 
 
